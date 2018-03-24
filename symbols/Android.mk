f9eb5a6920472456f1c0e7dd05e72ac2a767cdd0
DidntRead <koko1999@mail.bg>
2018-03-24 14:15:07 +0200

    move symbols to vendor

--- symbols/Android.mk
+++ symbols/Android.mk
@@ -4,5 +4,6 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := gui.cpp
 LOCAL_SHARED_LIBRARIES := libbinder libui libgui
 LOCAL_MODULE := libmtkshim_gui
+LOCAL_PROPRIETARY_MODULE := true
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
