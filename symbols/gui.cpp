01bbc3ec78d0c2e89c377e6853d3c80898a66509
DidntRead <koko1999@mail.bg>
2018-03-28 02:14:48 +0300

    update gui shim

--- symbols/gui.cpp
+++ symbols/gui.cpp
@@ -1,5 +1,12 @@
 #include <stdint.h>
+#include <string>
 
 extern "C" {
-    void _ZN7android13GraphicBufferC1Ejjij(uint32_t inWidth, uint32_t inHeight, int32_t inFormat, uint32_t inUsage);
+
+void _ZN7android13GraphicBufferC1EjjijNSt3__112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEE(uint32_t inWidth, uint32_t inHeight, int inFormat, uint32_t inUsage, std::string requestorName);
+
+void _ZN7android13GraphicBufferC1Ejjij(uint32_t inWidth, uint32_t inHeight, int inFormat, uint32_t inUsage) {
+    std::string requestorName = "<Unknown>";
+    _ZN7android13GraphicBufferC1EjjijNSt3__112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEE(inWidth, inHeight, inFormat, inUsage, requestorName);
+}
 }
